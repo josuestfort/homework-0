b6346e5a78575da3f2403bbe5a3b1c36854bd2ec
josuestfort <josuestfort@ufl.edu>
2023-11-28 09:42:20 -0500

    Modification for Fibonacci sequence

--- code.R
+++ code.R
@@ -1,1 +1,20 @@
 library(tidyverse)
+# Define a function to generate the n-th term of the fibonacci sequence
+fibonacci <- function(n) {
+  # Base cases
+  if (n == 0) {
+    return(0)
+  } else if (n == 1) {
+    return(1)
+  } else {
+    # Recursive case
+    return(fibonacci(n-1) + fibonacci(n-2))
+  }
+}
+# ask the user to enter a number
+n= readline(prompt=" Please enter the nth term of the sequence:")
+n= as.integer(n)
+# Loop through the first 20 terms and print them
+for (i in 0:n) {
+  cat(fibonacci(i), " ")
+}
